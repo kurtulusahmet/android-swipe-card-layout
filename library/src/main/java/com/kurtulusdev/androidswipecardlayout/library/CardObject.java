4b9e852886cc4b0aa0b9c7e9187d3a214171d347
kurtulusahmet <ahmettemel@kodpit.com>
2015-11-25 19:15:31 +0200

    added CardObject class

---
+++ library/src/main/java/com/kurtulusdev/androidswipecardlayout/library/CardObject.java
@@ -0,0 +1,25 @@
+package com.kurtulusdev.androidswipecardlayout.library;
+
+/**
+ * Created by kurtulusahmet on 25.11.2015.
+ */
+public class CardObject {
+    private String title;
+    private String image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
